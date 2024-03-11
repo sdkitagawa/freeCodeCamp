3458f00a1334ac1224589378e488a0ab7e82bf54
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2024-03-11 12:31:15 +0900

    Deleting Experimental Folder
    Deleting an experimental folder.

--- experimental/ham-menu-main/app.js
+++
@@ -1,8 +0,0 @@
-const hamMenu = document.querySelector(".ham-menu");
-
-const offScreenMenu = document.querySelector(".off-screen-menu");
-
-hamMenu.addEventListener("click", () => {
-  hamMenu.classList.toggle("active");
-  offScreenMenu.classList.toggle("active");
-});
