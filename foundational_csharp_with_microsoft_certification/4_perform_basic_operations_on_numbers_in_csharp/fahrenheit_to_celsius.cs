325b95d0243317c29033f5bccf75c7f1fe000d62
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2024-03-16 15:14:08 +0900

    Adding the Class 4 of C#
    Adding the class 4 file and the challenge.

---
+++ foundational_csharp_with_microsoft_certification/4_perform_basic_operations_on_numbers_in_csharp/fahrenheit_to_celsius.cs
@@ -0,0 +1,4 @@
+// Fahrenheit to Celsius
+int currentFahrenheit = 94;
+decimal fahrenheitToCelsius = (currentFahrenheit - 32m) * 5m / 9m;
+Console.WriteLine($"The Fahrenheit temperature is {currentFahrenheit}º F.\nThe Celsius temperature is {fahrenheitToCelsius}º C.");
