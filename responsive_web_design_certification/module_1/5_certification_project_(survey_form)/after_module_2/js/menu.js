9d5af82a99ece10bc33741f9421eeb3e6718cb58
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2024-04-08 13:08:38 +0900

    Remove experimental project

--- responsive_web_design_certification/module_1/5_certification_project_(survey_form)/after_module_2/js/menu.js
+++
@@ -1,7 +0,0 @@
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-const offScreenMenu = document.querySelector(".off-screen-menu");
-
-hamburgerMenu.addEventListener("click", () => {
-  hamburgerMenu.classList.toggle("active");
-  offScreenMenu.classList.toggle("active");
-});
